8a04bfe9c7c457ddfa79bfdc4da4ea0cfc0a2ac7
Syed Shaeduzzaman Noor <88043909+syednoor058@users.noreply.github.com>
2024-03-06 16:15:46 +0600

    Update Home.js http to https

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -28,7 +28,7 @@ export default function Home() {
     //   });
 
     try {
-      const response = await fetch("http://hungry-shaed-mern-stack-api.vercel.app/api/food-data", {
+      const response = await fetch("https://hungry-shaed-mern-stack-api.vercel.app/api/food-data", {
         method: "POST",
       });
       if (response.ok) {
